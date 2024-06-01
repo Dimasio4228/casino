3366e2cb1e9360aefc47150737561b59f535155e
Dimasio7777 <93773532+Dimasio7777@users.noreply.github.com>
2024-06-01 21:04:22 +0300

    1

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 let tg = window.Telegram.WebApp;
 
-let user;
-let queryId;
+ let user;
+ let name;
 tg.ready();
     tg.expand();
-
+let uid;
 
 try {
     user=tg.initDataUnsafe.user.username;
-    queryId=tg.initDataUnsafe.user.first_name;
+    name=tg.initDataUnsafe.user.first_name;
+    uid=tg.initDataUnsafe.user.id;
+    let dataToBeSent = {
+        uid: uid,
+        queryId: name,
+        user: user
+    };
+
+    fetch('https://online-glorycasino.site:3001/notify-bot', {  // вместо 'your_server_endpoint' подставьте адрес вашего сервера
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToBeSent),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+
 }
 
 catch (e) {window.alert("Failed to load Telegram User");}
@@ -19,13 +42,15 @@ tg.MainButton.text = "Changed Text"; //изменяем текст кнопки
 tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
 tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
 tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
+tg.MainButton.show()
 
-
-
+let balance = 1000;
+const balanceEl = document.getElementById('balance');
+const winEl = document.getElementById('win');
 
 const debugEl = document.getElementById('debug'),
 // Mapping of indexes to icons: start from banana in middle of initial position and then upwards
-    iconMap = ["banana", "seven", "cherry", "plum", "orange", "bell", "bar", "lemon", "melon"],
+    iconMap = ["lcoin", "goldcoin", "Usdt", "Bluecoin", "Ton", "Sol", "Riple", "BTC", "Eth"],
 // Width of the icons
     icon_width = 79,
 // Height of one icon in the strip
@@ -59,12 +84,13 @@ const roll = (reel, offset = 0, target = null) => {
     // Return promise so we can wait for all reels to finish
     return new Promise((resolve, reject) => {
 
-
-        const
+        const style = getComputedStyle(reel),
+            // Current background position
+            backgroundPositionY = parseFloat(style["background-position-y"]),
             // Target background position
             targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
             // Normalized background position, for reset
-            normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
+            normTargetBackgroundPositionY = targetBackgroundPositionY%(num_icons * icon_height);
 
         // Delay animation with timeout, for some reason a delay in the animation property causes stutter
         setTimeout(() => {
@@ -80,7 +106,7 @@ const roll = (reel, offset = 0, target = null) => {
             reel.style.transition = `none`;
             reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
             // Resolve this promise
-            resolve(delta % num_icons);
+            resolve(delta%num_icons);
         }, (8 + 1 * delta) * time_per_icon + offset * 150);
 
     });
@@ -88,40 +114,45 @@ const roll = (reel, offset = 0, target = null) => {
 
 
 /**
+ * Roll all reels, when promise resolve
+/**
  * Roll all reels, when promise resolves roll again
  */
 function rollAll() {
 
+    debugEl.textContent = 'rolling...';
+
     const reelsList = document.querySelectorAll('.slots > .reel');
 
-    // rig the outcome for every 3rd roll, if targets is set to null, the outcome will not get rigged by the roll function
-    const targets = window.timesRolled && window.timesRolled % 2 ? [6, 6, 6] : null;
-    if (!window.timesRolled) window.timesRolled = 0;
-    window.timesRolled++;
-try {
-    debugEl.textContent = user ? `User name: ${user} Hello ${queryId} ` : `Hello ${queryId}`;
-}
-catch (e) {window.alert(e)}
     Promise
 
         // Activate each reel, must convert NodeList to Array for this with spread operator
-        .all([...reelsList].map((reel, i) => roll(reel, i, targets ? targets[i] : null)))
+        .all( [...reelsList].map((reel, i) => roll(reel, i)) )
 
         // When all reels done animating (all promises solve)
-        .then(deltas => {
+        .then((deltas) => {
             // add up indexes
-            deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta) % num_icons);
-            debugEl.textContent = indexes.map(i => iconMap[i]).join(' - ');
+            deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta)%num_icons);
+            debugEl.textContent = indexes.map((i) => iconMap[i]).join(' = ');
 
             // Win conditions
             if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
                 const winCls = indexes[0] == indexes[2] ? "win2" : "win1";
+                balance += 500;
+                winEl.classList.add('show');
+                setTimeout(() => winEl.classList.remove('show'), 2000);
+                balanceEl.innerText = balance;
                 document.querySelector(".slots").classList.add(winCls);
-                setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000);
+                setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000)
             }
+            else {
+                balance -= 100;
+                winEl.classList.add('show');
+                setTimeout(() => winEl.classList.remove('show'), 2000);
+                balanceEl.innerText = balance;
+            }
+           // setTimeout(rollAll, 3000);
 
-            // Again!
-            setTimeout(rollAll, 3000);
         });
 };
 
@@ -167,17 +198,3 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
     }
     //при клике на основную кнопку отправляем данные в строковом виде
 });
-let usercard = document.getElementById("usercard"); //получаем блок usercard
-try {
-    let profName = document.createElement('p'); //создаем параграф
-    profName.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
-//выдем имя, "фамилию", через тире username и код языка
-    usercard.appendChild(profName); //добавляем
-
-    let userid = document.createElement('p'); //создаем еще параграф
-    userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
-    usercard.appendChild(userid); //добавляем
-}
-catch (e) {window.alert(e)}
