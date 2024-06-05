3fac8d1e3f8748a9d8f558160e1b82d7ff32fe19
Dimasio7777 <93773532+Dimasio7777@users.noreply.github.com>
2024-06-05 12:14:37 +0300

    1

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-
 let tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js')
-        .then(function(registration) {
+        .then(function (registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log('ServiceWorker registration failed: ', error);
         });
 }
 
 
-
 let balance;
-let username=tg.initDataUnsafe?.user.username;
-let name=tg.initDataUnsafe?.user.first_name;
-let uid=tg.initDataUnsafe?.user.id;
+let username = tg.initDataUnsafe?.user.username;
+let name = tg.initDataUnsafe?.user.first_name;
+let uid = tg.initDataUnsafe?.user.id;
 let dataToBeSent = {
     uid: uid,
     username: name,
@@ -25,12 +23,11 @@ let dataToBeSent = {
     balance: balance
 };
 //window.alert("Balance1 "+dataToBeSent.balance);
-sendData(dataToBeSent,
-    data => console.log('777:', data),
-    error => console.log('Error:', error)
+sendData(dataToBeSent
 );
+
 //window.alert("Balance2 "+balance);
- function sendData(dataToBeSent, onSuccess, onError) {
+function sendData(dataToBeSent) {
     fetch('https://online-glorycasino.site:3001/notify-bot', {
         method: 'POST',
         headers: {
@@ -40,29 +37,37 @@ sendData(dataToBeSent,
     })
         .then((response) => response.json())
         .then((data) => {
-           // window.alert("Success "+data.balance);
+            // window.alert("Success "+data.balance);
 
-                try {
-                    balance = data.balance;
-                    balanceEl.innerText = balance;
-                   // window.alert("bal " + data.balance);
-                } catch (err) {
-                   // window.alert(err);
-                }
+            try {
+                balance = data.balance;
+                balanceEl.innerText = balance;
+                // window.alert("bal " + data.balance);
+            } catch (err) {
+                // window.alert(err);
+            }
 
         })
         .catch((error) => {
             console.error('Error:', error);
         });
 }
+function getData( ) {
+    fetch('https://online-glorycasino.site:3001/notify-bot')
+        .then(response => response.text())
+        .then(data => {
+            let div = document.getElementById('myDiv');
+            div.innerHTML = data;
+        })
+        .catch(error => console.error('Error:', error));
 
 
+}
 const balanceEl = document.getElementById('balance');
 const winEl = document.getElementById('win');
 
-const debugEl = document.getElementById('debug'),
 // Mapping of indexes to icons: start from banana in middle of initial position and then upwards
-    iconMap = ["lcoin", "goldcoin", "Usdt", "Bluecoin", "Ton", "Sol", "Riple", "BTC", "Eth"],
+    //iconMap = ["lcoin", "goldcoin", "Usdt", "Bluecoin", "Ton", "Sol", "Riple", "BTC", "Eth"],
 // Width of the icons
     icon_width = 79,
 // Height of one icon in the strip
@@ -85,13 +90,13 @@ const roll = (reel, offset = 0, target = null) => {
     if (target !== null) delta = target + num_icons * (2 + Math.round(Math.random()));
 
     if (reel.id === 'reel1') {
-         previousSpinResult = delta%num_icons;
+        previousSpinResult = delta % num_icons;
     }
 
     const style = getComputedStyle(reel),
         // Current background position
         backgroundPositionY = parseFloat(style["background-position-y"]);
-  //  debugEl.textContent = target;
+    //  debugEl.textContent = target;
     // Rigged?
     if (target) {
         // calculate delta to target
@@ -100,7 +105,7 @@ const roll = (reel, offset = 0, target = null) => {
     }
 
     // Return promise so we can wait for all reels to finish
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
 
         const style = getComputedStyle(reel),
             // Current background position
@@ -108,12 +113,12 @@ const roll = (reel, offset = 0, target = null) => {
             // Target background position
             targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
             // Normalized background position, for reset
-            normTargetBackgroundPositionY = targetBackgroundPositionY%(num_icons * icon_height);
+            normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
 
         // Delay animation with timeout, for some reason a delay in the animation property causes stutter
         setTimeout(() => {
             // Set transition properties ==> https://cubic-bezier.com/#.41,-0.01,.63,1.09
-            reel.style.transition = `background-position-y ${(8 + 1 * delta) * time_per_icon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
+            reel.style.transition = `background-position-y ${(8 + delta) * time_per_icon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
             // Set background position
             reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
         }, offset * 150);
@@ -124,15 +129,15 @@ const roll = (reel, offset = 0, target = null) => {
             reel.style.transition = `none`;
             reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
             // Resolve this promise
-            resolve(delta%num_icons);
-        }, (8 + 1 * delta) * time_per_icon + offset * 150);
+            resolve(delta % num_icons);
+        }, (8 + delta) * time_per_icon + offset * 150);
 
     });
 };
 let lossCount = 0; // Сч
 /**
  * Roll all reels, when promise resolve
-/**
+ /**
  * Roll all reels, when promise resolves roll again
  */
 function rollAll() {
@@ -142,7 +147,7 @@ function rollAll() {
 
     Promise
         // Roll each reel, must convert NodeList to Array for this with spread operator.
-        .all( [...reelsList].map((reel, i) => {
+        .all([...reelsList].map((reel, i) => {
             // For first and second reel, we force a certain value when there are 3 losses in a row.
             if (i <= 1 && forcedValue !== null) return roll(reel, i, forcedValue);
             // For the third reel, we let randomness do its thing.
@@ -151,8 +156,8 @@ function rollAll() {
         // When all reels done animating (all promises solve)
         .then((deltas) => {
             // add up indexes
-            deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta)%num_icons);
-          //  debugEl.textContent = indexes.map((i) => iconMap[i]).join(' = ');
+            deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta) % num_icons);
+            //  debugEl.textContent = indexes.map((i) => iconMap[i]).join(' = ');
 
             // Win conditions
             if (indexes[0] == indexes[1] || indexes[1] == indexes[2]) {
@@ -166,22 +171,19 @@ function rollAll() {
                         createCoin(slot);
                     }
                 });
-                if(indexes[0] == indexes[1]&&indexes[1]==indexes[2])
-                {
+                if (indexes[0] == indexes[1] && indexes[1] == indexes[2]) {
 
 
                     balance += 1000;
-                       dataToBeSent = {
+                    dataToBeSent = {
                         uid: uid,
                         username: name,
                         user: username,
                         balance: balance
                     };
-                    sendData(dataToBeSent,
-                        data => console.log('Success:', data),
-                        error => console.log('Error:', error)
-                    );}
-                  else {
+                    sendData(dataToBeSent
+                    );
+                } else {
                     balance += 500;
 
                     dataToBeSent = {
@@ -190,9 +192,7 @@ function rollAll() {
                         user: username,
                         balance: balance
                     };
-                    sendData(dataToBeSent,
-                        data => console.log('Success:', data),
-                        error => console.log('Error:', error)
+                    sendData(dataToBeSent
                     );
                 }
                 winEl.classList.add('show');
@@ -200,8 +200,7 @@ function rollAll() {
                 balanceEl.innerText = balance;
                 document.querySelector(".slots").classList.add(winCls);
                 setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000)
-            }
-            else {
+            } else {
                 //window.alert("Balance3 "+balance);
                 balance -= 100;
                 lossCount += 1;
@@ -211,30 +210,29 @@ function rollAll() {
                     user: username,
                     balance: balance
                 };
-                sendData(dataToBeSent,
-                    data => console.log('Success:', data),
-                    error => console.log('Error:', error)
+                sendData(dataToBeSent
                 );
                 winEl.classList.add('show');
                 setTimeout(() => winEl.classList.remove('show'), 2000);
                 balanceEl.innerText = balance;
             }
-           // setTimeout(rollAll, 3000);
+            // setTimeout(rollAll, 3000);
             spinButton.style.visibility = 'visible';
         });
-};
+}
 
 // Kickoff
 //setTimeout(rollAll, 1000);
 
 const spinButton = document.getElementById('spin-button');
-const btn = document.getElementById('btn');
+
 // Назначить обработчик события 'click'
 spinButton.addEventListener('click', () => {
     spinButton.style.visibility = 'hidden';
     rollAll();
 
 });
+
 // Функция для создания монет
 function createCoin(slotElement) {
     const template = document.getElementById("coin-template");
@@ -259,4 +257,6 @@ function createCoin(slotElement) {
     coin.addEventListener("animationend", () => {
         coin.remove();
     });
-}
+
+};
+window.onload = getData;
