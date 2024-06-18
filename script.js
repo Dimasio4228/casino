0a46a376ce0c6acd853383c5359e55598147d694
Dimasio7777 <93773532+Dimasio7777@users.noreply.github.com>
2024-06-18 16:22:10 +0300

    Razmetka Rel.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-const {createCoin}=require('./coin');
-
 let tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
@@ -110,6 +107,8 @@ const roll = (reel, offset = 0, target = null) => {
         const currentIndex = backgroundPositionY / icon_height;
         delta = target - currentIndex + (offset + 2) * num_icons;
     }
+
+    // Return promise so we can wait for all reels to finish
     return new Promise((resolve) => {
 
         const style = getComputedStyle(reel),
@@ -176,6 +175,8 @@ function rollAll()  {
                     }
                 });
                 if (indexes[0] === indexes[1] && indexes[1] === indexes[2]) {
+
+
                     balance += 1000;
                     dataToBeSent = {
                         uid: uid,
@@ -255,6 +256,31 @@ catch (e) {
 });
 
 // Функция для создания монет
+function createCoin(slotElement) {
+    const template = document.getElementById("coin-template");
+    const coin = template.cloneNode(true);
+    coin.id = ""; // сброс ID
+    coin.hidden = false; // делаем видимым
+
+    // Получить позицию и размеры слота
+    const slotRect = slotElement.getBoundingClientRect();
+
+    // Задать начальное положение монеты из середины слота
+    coin.style.left = `${slotRect.left + slotRect.width / 2}px`;
+    coin.style.top = `${slotRect.top + slotRect.height / 2}px`;
+
+    // Задать различные траектории полета монет (можно экспериментировать с этими значениями)
+    coin.style.setProperty("animation-duration", `${2 + Math.random()}s`);
+    coin.style.setProperty("animation-timing-function", `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`);
+
+    document.body.appendChild(coin); // Add new coin to DOM
+
+    // Удалить монету из DOM после окончания анимации
+    coin.addEventListener("animationend", () => {
+        coin.remove();
+    });
+
+}
 
 
 
