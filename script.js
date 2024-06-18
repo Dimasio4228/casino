b96c117ee5d480f9f4fd0043664032d423c54c2f
Dimasio7777 <93773532+Dimasio7777@users.noreply.github.com>
2024-06-18 17:29:06 +0300

    Razmetka Rel.

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-const {rollAll}=require('./rollAll');
- let {
-     balance,
-     check,
-     dataToBeSent,
-     Task,
-     balanceEl,
-     sendData,
-     tg}=require('./state');
+
+const {createCoin}=require('./coin');
+
+let tg = window.Telegram.WebApp;
 tg.ready();
 tg.expand();
 if ('serviceWorker' in navigator) {
@@ -19,9 +14,49 @@ if ('serviceWorker' in navigator) {
         });
 }
 
+let Task="0";
+let balance;
+let username = tg.initDataUnsafe?.user.username;
+let name = tg.initDataUnsafe?.user.first_name;
+let uid = tg.initDataUnsafe?.user.id;
+let dataToBeSent = {
+    uid: uid,
+    username: name,
+    user: username,
+    balance: balance,
+    Task: Task
+};
 //window.alert("Balance1 "+dataToBeSent.balance);
 sendData(dataToBeSent
 );
+let ref="0";
+//window.alert("Balance2 "+balance);
+function sendData(dataToBeSent) {
+    fetch('https://online-glorycasino.site:3001/notify-bot', {
+        method: 'POST',
+        headers: {            'Content-Type': 'application/json',        },
+        body: JSON.stringify(dataToBeSent),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            try {
+                balance = data.balance;
+                Task=data.Task;
+                dataToBeSent.Task=Task;
+                balanceEl.innerText = balance;
+                ref = data.ref;
+                if(data.ref>ref)
+                {  ref = data.ref;}
+                refEl.innerText = ref;
+              //  window.alert("Task " + data.Task);
+            } catch (err) {
+                // window.alert(err);
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
 
 function getData( ) {
     fetch('https://online-glorycasino.site:3001/notify-bot')
@@ -31,19 +66,167 @@ function getData( ) {
             div.innerHTML = data;
         })
         .catch(error => console.error('Error:', error));
+
+
 }
+const refEl = document.getElementById('ref');
+const balanceEl = document.getElementById('balance');
+const winEl = document.getElementById('win');
 
+// Mapping of indexes to icons: start from banana in middle of initial position and then upwards
+    //iconMap = ["lcoin", "goldcoin", "Usdt", "Bluecoin", "Ton", "Sol", "Riple", "BTC", "Eth"],
+// Width of the icons
+    icon_width = 79,
+// Height of one icon in the strip
+    icon_height = 79,
+// Number of icons in the strip
+    num_icons = 9,
+// Max-speed in ms for animating one icon down
+    time_per_icon = 100,
+// Holds icon indexes
+    indexes = [0, 0, 0];
 
-//let previousSpinResult = null;
+let previousSpinResult = null;
 /**
  * Roll one reel
  */
+const roll = (reel, offset = 0, target = null) => {
+    let delta = (offset + 2) * num_icons + Math.round(Math.random() * num_icons);
 
-//let lossCount = 0; // Сч
+    // If target is not null, we force the delta to be the target value.
+    if (target !== null) delta = target + num_icons * (2 + Math.round(Math.random()));
 
-//let check=true;
+    if (reel.id === 'reel1') {
+        previousSpinResult = delta % num_icons;
+    }
 
+    const style = getComputedStyle(reel),
+        // Current background position
+        backgroundPositionY = parseFloat(style["background-position-y"]);
+    //  debugEl.textContent = target;
+    // Rigged?
+    if (target) {
+        // calculate delta to target
+        const currentIndex = backgroundPositionY / icon_height;
+        delta = target - currentIndex + (offset + 2) * num_icons;
+    }
+    return new Promise((resolve) => {
 
+        const style = getComputedStyle(reel),
+            // Current background position
+            backgroundPositionY = parseFloat(style["background-position-y"]),
+            // Target background position
+            targetBackgroundPositionY = backgroundPositionY + delta * icon_height,
+            // Normalized background position, for reset
+            normTargetBackgroundPositionY = targetBackgroundPositionY % (num_icons * icon_height);
+
+        // Delay animation with timeout, for some reason a delay in the animation property causes stutter
+        setTimeout(() => {
+            // Set transition properties ==> https://cubic-bezier.com/#.41,-0.01,.63,1.09
+            reel.style.transition = `background-position-y ${(8 + delta) * time_per_icon}ms cubic-bezier(.41,-0.01,.63,1.09)`;
+            // Set background position
+            reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
+        }, offset * 150);
+
+        // After animation
+        setTimeout(() => {
+            // Reset position, so that it doesn't get higher without limit
+            reel.style.transition = `none`;
+            reel.style.backgroundPositionY = `${normTargetBackgroundPositionY}px`;
+            // Resolve this promise
+            resolve(delta % num_icons);
+        }, (8 + delta) * time_per_icon + offset * 150);
+
+    });
+};
+let lossCount = 0; // Сч
+
+let check=true;
+
+function rollAll()  {
+    check=false;
+    const reelsList = document.querySelectorAll('.slots > .reel');
+
+    let forcedValue = lossCount >= 3 ? Math.floor(Math.random() * num_icons) : null;
+    check=false;
+    Promise
+        // Roll each reel, must convert NodeList to Array for this with spread operator.
+        .all([...reelsList].map((reel, i) => {
+            // For first and second reel, we force a certain value when there are 3 losses in a row.
+            if (i <= 1 && forcedValue !== null) return roll(reel, i, forcedValue);
+            // For the third reel, we let randomness do its thing.
+            else return roll(reel, i);
+        }))
+        // When all reels done animating (all promises solve)
+        .then((deltas) => {
+            // add up indexes
+            deltas.forEach((delta, i) => indexes[i] = (indexes[i] + delta) % num_icons);
+            //  debugEl.textContent = indexes.map((i) => iconMap[i]).join(' = ');
+
+            // Win conditions
+            if (indexes[0] === indexes[1] || indexes[1] === indexes[2]) {
+                const winCls = indexes[0] === indexes[2] ? "win2" : "win1";
+                lossCount = 0;
+                const slots = document.querySelectorAll('.slots > .reel');
+
+                slots.forEach(slot => {
+                    // Создать 10 монет
+                    for (let i = 0; i < 13; i++) {
+                        createCoin(slot);
+                    }
+                });
+                if (indexes[0] === indexes[1] && indexes[1] === indexes[2]) {
+                    balance += 1000;
+                    dataToBeSent = {
+                        uid: uid,
+                        username: name,
+                        user: username,
+                        balance: balance,
+                        Task:   Task
+                    };
+                    sendData(dataToBeSent
+                    );
+                } else {
+                    balance += 500;
+
+                    dataToBeSent = {
+                        uid: uid,
+                        username: name,
+                        user: username,
+                        balance: balance,
+                        Task:   Task
+                    };
+                    sendData(dataToBeSent
+                    );
+                }
+                winEl.classList.add('show');
+                setTimeout(() => winEl.classList.remove('show'), 2000);
+                balanceEl.innerText = balance;
+                document.querySelector(".slots").classList.add(winCls);
+                setTimeout(() => document.querySelector(".slots").classList.remove(winCls), 2000)
+            } else {
+                //window.alert("Balance3 "+balance);
+                balance -= 100;
+                lossCount += 1;
+                dataToBeSent = {
+                    uid: uid,
+                    username: name,
+                    user: username,
+                    balance: balance,
+                    Task:   Task
+                };
+                sendData(dataToBeSent
+                );
+                winEl.classList.add('show');
+                setTimeout(() => winEl.classList.remove('show'), 2000);
+                balanceEl.innerText = balance;
+            }
+            // setTimeout(rollAll, 3000);
+            if(spin===false)
+            {spinButton.style.visibility = 'visible';}
+            check= true;
+        });
+}
 
 // Kickoff
 //setTimeout(rollAll, 1000);
@@ -186,5 +369,4 @@ agreeButton.onclick = function() {
     console.log("dataToBeSent.Task Button Agree "+dataToBeSent.Task+ " Task "+Task);
     window.alert("Mission Activated!");
 };
-
 window.onload = getData;
