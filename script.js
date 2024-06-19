11c557720a78b1bb4179105b049a9ea2c972d58f
Dimasio7777 <93773532+Dimasio7777@users.noreply.github.com>
2024-06-20 00:17:56 +0300

    Razmetka Rel.

--- script.js
+++ script.js
@@ -141,8 +141,32 @@ const roll = (reel, offset = 0, target = null) => {
     });
 };
 let lossCount = 0; // Сч
+function createCoin(slotElement) {
+    const template = document.getElementById("coin-template");
+    const coin = template.cloneNode(true);
+    coin.id = ""; // сброс ID
+    coin.hidden = false; // делаем видимым
 
-//let check=true;
+    // Получить позицию и размеры слота
+    const slotRect = slotElement.getBoundingClientRect();
+
+    // Задать начальное положение монеты из середины слота
+    coin.style.left = `${slotRect.left + slotRect.width / 2}px`;
+    coin.style.top = `${slotRect.top + slotRect.height / 2}px`;
+
+    // Задать различные траектории полета монет (можно экспериментировать с этими значениями)
+    coin.style.setProperty("animation-duration", `${2 + Math.random()}s`);
+    coin.style.setProperty("animation-timing-function", `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`);
+
+    document.body.appendChild(coin); // Add new coin to DOM
+
+    // Удалить монету из DOM после окончания анимации
+    coin.addEventListener("animationend", () => {
+        coin.remove();
+    });
+
+}
+ let check=true;
 
 function rollAll()  {
     check=false;
